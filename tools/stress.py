2077c43ee31bd6e7d143e5b0c788c539f1b240e7
Gianni_Home <gianni.lodetti@hotmail.com>
2021-11-28 02:13:03 +0100

    limit number of threads... however broadcast way too slow when many messages.... could have some algorithm problems, URB seems to be the problem and i pray FIFO and Causal will work after i fix URB

--- tools/stress.py
+++ tools/stress.py
@@ -362,11 +362,11 @@ if __name__ == "__main__":
 
     testConfig = {
         'concurrency' : 1, # How many threads are interferring with the running processes //had 8 here
-        'attempts' : 10, # How many interferring attempts each threads does //had 100 here
+        'attempts' : 1, # How many interferring attempts each threads does //had 100 here
         'attemptsDistribution' : { # Probability with which an interferring thread will
-            'STOP': 0.60,          # select an interferring action (make sure they add up to 1)
+            'STOP': 0.50,          # select an interferring action (make sure they add up to 1)
             'CONT': 0.20,
-            'TERM':0.2
+            'TERM':0.3
         }
     }
 
